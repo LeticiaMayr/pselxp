136e413a17c6aa7e8be7afa11ba20477889d87fa
Leticia <fmsornellas@gmail.com>
2022-07-16 23:28:40 -0300

    feat: confirm button completly functional

--- frontend/pselxp-front/src/components/ConfirmButton.js
+++ frontend/pselxp-front/src/components/ConfirmButton.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useProvider } from "../context/provider";
 
 function ConfirmButton() {
+  const { balance, setBalance, selectedService, value } = useProvider();
+
+  function handleAccount() {
+    if (typeof value === 'string' && value !== '') {
+      if (selectedService === 'deposit') {
+        const untreatedBalance = balance + parseFloat(value);
+        const newBalance = Math.round(untreatedBalance * 100)/100;
+        setBalance(newBalance);
+      };
+      if (selectedService === 'withdraw' && balance >= value) {
+        const untreatedBalance = balance - parseFloat(value);
+        const newBalance = Math.round(untreatedBalance * 100)/100;
+        setBalance(newBalance);
+      };
+    };
+    if (selectedService === 'withdraw' && balance < value) alert('Insira um valor adequado');
+  }
+
   return (
-    <div>
+    <button
+      onClick={ handleAccount }
+    >
       Confirmar
-    </div>
+    </button>
   );
 };
 
