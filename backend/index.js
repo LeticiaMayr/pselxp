27beda88d6677eb148f14e1ef58b4fb2ff9c3406
Leticia <fmsornellas@gmail.com>
2022-07-17 01:13:01 -0300

    fix: routes for stocks and clients to feed front end done

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
-import express from 'express';
+const express = require('express');
 
 const { readFileSync } = require('fs');
 
 // Following functions simulate connection with database
 
-const getClients = async () => JSON.parse(readFileSync('./backend/clients.json'));
-const getStocks = async () => JSON.parse(readFileSync('./backend/stocks.json'));
+const getClients = async () => JSON.parse(readFileSync('./clients.json'));
+const getStocks = async () => JSON.parse(readFileSync('./stocks.json'));
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/clients', (_request, response) => {
-  response.status(200).send(getClients());
+app.get('/clients', async (_request, response) => {
+  const clients = await getClients();
+  response.status(200).send(clients);
 });
 
-app.get('/stocks', (_request, response) => {
-  response.status(200).send(getStocks());
+app.get('/stocks', async (_request, response) => {
+  const stocks = await getStocks();
+  response.status(200).send(stocks);
 });
 
 app.listen(3009, () => {
