ed49aa1b0079cdbca3c1d24ecfef1ec352b4db1f
Leticia <fmsornellas@gmail.com>
2022-07-16 23:26:59 -0300

    feat: provider and account with all functionalities, still needing to change confirm button though

--- frontend/pselxp-front/src/context/provider.js
+++ frontend/pselxp-front/src/context/provider.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const mainContext = React.createContext({});
 
 export const GeneralProvider = (props) => {
+  const [balance, setBalance] = useState(0);
+  const [value, setValue] = useState('');
+  const [selectedService, setSelectedService] = useState('deposit');
 
   return (
     <mainContext.Provider value={
-      {}
+      {
+        balance, setBalance,
+        value, setValue,
+        selectedService, setSelectedService
+      }
     }>
       {props.children}
     </mainContext.Provider>
--- frontend/pselxp-front/src/pages/Account.js
+++ frontend/pselxp-front/src/pages/Account.js
@@ -1,13 +1,45 @@
-import React from "react";
-import Header from "../components/Header";
+import React from 'react';
+import Header from '../components/Header';
+import BackButton from '../components/BackButton';
+import ConfirmButton from '../components/ConfirmButton';
+import { useProvider } from '../context/provider';
 
 function Account() {
+  const { balance, setValue, setSelectedService, value } = useProvider()
+
+  function handleChange({ target }) {
+    setValue(target.value);
+  }
+
   return (
     <>
-      <Header />
+      <Header/>
       <div>
-        Conta
+        Saldo em conta: {balance}
       </div>
+      <div>
+        <button
+          id='deposit'
+          onClick={ () => setSelectedService('deposit') }
+        >
+          Depositar
+        </button>
+        <button
+          id='withdraw'
+          onClick={ () => setSelectedService('withdraw') }
+        >
+          Retirada
+        </button>
+      </div>
+      <input
+        type='number'
+        placeholder='informe o valor'
+        step={0.01}
+        onChange={ handleChange }
+        value={ value }
+      />
+      <BackButton />
+      <ConfirmButton />
     </>
   );
 };
