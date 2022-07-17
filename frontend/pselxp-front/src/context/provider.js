be189c8b612fe340325fee7aa1bb8a22c6dc77f6
Leticia <fmsornellas@gmail.com>
2022-07-17 01:16:35 -0300

    chore: state to use for stocks on their page done

--- frontend/pselxp-front/src/context/provider.js
+++ frontend/pselxp-front/src/context/provider.js
@@ -6,13 +6,15 @@ export const GeneralProvider = (props) => {
   const [balance, setBalance] = useState(0);
   const [value, setValue] = useState('');
   const [selectedService, setSelectedService] = useState('deposit');
+  const [stockList, setStockList] = useState([]);
 
   return (
     <mainContext.Provider value={
       {
         balance, setBalance,
         value, setValue,
-        selectedService, setSelectedService
+        selectedService, setSelectedService,
+        stockList, setStockList
       }
     }>
       {props.children}
