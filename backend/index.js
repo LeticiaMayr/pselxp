43ac459fb5c0a2060a2e06635857baf0e9414087
Leticia <fmsornellas@gmail.com>
2022-07-17 00:34:13 -0300

    chore: defined routes for routes im using for now

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,25 @@
+import express from 'express';
+
+
 const { readFileSync } = require('fs');
 
-const readFile = async () => JSON.parse(readFileSync('./backend/clients.json'));
+// Following functions simulate connection with database
 
-module.exports = readFile;
+const getClients = async () => JSON.parse(readFileSync('./backend/clients.json'));
+const getStocks = async () => JSON.parse(readFileSync('./backend/stocks.json'));
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/clients', (_request, response) => {
+  response.status(200).send(getClients());
+});
+
+app.get('/stocks', (_request, response) => {
+  response.status(200).send(getStocks());
+});
+
+app.listen(3009, () => {
+  console.log('Online');
+});
