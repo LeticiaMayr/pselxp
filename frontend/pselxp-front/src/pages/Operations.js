307e82d3768ff71f6e34ea83559997665dcf8ca1
Leticia <fmsornellas@gmail.com>
2022-07-17 22:00:54 -0300

    feat: back button added to operations page

--- frontend/pselxp-front/src/pages/Operations.js
+++ frontend/pselxp-front/src/pages/Operations.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import BackButton from '../components/BackButton';
 
 function Operations() {
   return (
@@ -8,6 +9,7 @@ function Operations() {
       <div>
         Operações
       </div>
+      <BackButton />
     </>
   );
 };
