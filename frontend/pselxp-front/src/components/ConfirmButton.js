b0a9f78aad6f60cb0dfb494d2a23cf684a1d56dc
Leticia <fmsornellas@gmail.com>
2022-07-19 18:49:08 -0300

    feat: confirm button now connected to database

--- frontend/pselxp-front/src/components/ConfirmButton.js
+++ frontend/pselxp-front/src/components/ConfirmButton.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { useProvider } from "../context/provider";
+import axios from "axios";
 
 function ConfirmButton() {
-  const { balance, setBalance, selectedService, value } = useProvider();
+  const { balance, setBalance, selectedService, value, clientData } = useProvider();
 
   function handleAccount() {
+    const { id } = clientData;
     if (typeof value === 'string' && value !== '') {
       if (selectedService === 'deposit') {
         const untreatedBalance = balance + parseFloat(value);
         const newBalance = Math.round(untreatedBalance * 100)/100;
-        setBalance(newBalance);
+
+        axios.put(`http://localhost:3009/account/${id}`, { newBalance })
+        .then(res => {
+          setBalance(res.data.newBalance);
+        }).catch(err => {
+          console.log(err);
+        });
       };
       if (selectedService === 'withdraw' && balance >= value) {
         const untreatedBalance = balance - parseFloat(value);
         const newBalance = Math.round(untreatedBalance * 100)/100;
-        setBalance(newBalance);
+
+        axios.put(`http://localhost:3009/account/${id}`, { newBalance })
+        .then(res => {
+          setBalance(res.data.newBalance);
+        }).catch(err => {
+          console.log(err);
+        });
       };
     };
     if (selectedService === 'withdraw' && balance < value) alert('Insira um valor adequado');
