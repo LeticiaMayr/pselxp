2ed879d3431e058deba276e0a27ee4ceb0582d42
Leticia <fmsornellas@gmail.com>
2022-07-21 13:52:27 -0300

    refactor: sweetalert used to show beautiful alerts <3

--- frontend/pselxp-front/src/components/ConfirmButton.js
+++ frontend/pselxp-front/src/components/ConfirmButton.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { useProvider } from "../context/provider";
 import axios from "axios";
+import swal from "sweetalert";
 
 function ConfirmButton() {
   const { balance, selectedService, value, clientData, setClientData } = useProvider();
 
   function handleAccount() {
-    if (selectedService === 'withdraw' && balance < value) alert('Insira um valor abaixo ou igual ao seu saldo!');
-    if (value === '') alert('Insira algum valor para depósito ou saque!');
-    if (value <= 0 && value !== '') alert('Insira algum valor maior que 0!');
+    if (selectedService === 'withdraw' && balance < value) {
+      swal({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Insira algum valor menor ou igual que seu saldo!',
+      });
+    };
+
+    if (value === '') {
+      swal({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Insira algum valor para depósito ou saque!',
+      });
+    };
+
+    if (value <= 0 && value !== '') {
+      swal({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Insira algum valor maior que 0!',
+      });
+    };
 
     const { id } = clientData;
 
-    if (typeof value === 'string' && value !== '') {
+    if (typeof value === 'string' && value !== '' && value > 0) {
 
       if (selectedService === 'deposit') {
         const untreatedBalance = balance + parseFloat(value);
@@ -24,9 +45,15 @@ function ConfirmButton() {
         }).catch(err => {
           console.log(err);
         });
+
+        swal({
+          icon: 'success',
+          title: 'Seu depósito foi efetuado com sucesso!',
+          timer: 2500,
+        });
       };
 
-      if (selectedService === 'withdraw') {
+      if (selectedService === 'withdraw' && balance > value) {
         const untreatedBalance = balance - parseFloat(value);
         const newBalance = Math.round(untreatedBalance * 100)/100;
 
@@ -35,6 +62,12 @@ function ConfirmButton() {
           setClientData(res.data);
         }).catch(err => {
           console.log(err);
+        });
+
+        swal({
+          icon: 'success',
+          title: 'Seu saque foi efetuado com sucesso!',
+          timer: 2500,
         });
       };
     };
