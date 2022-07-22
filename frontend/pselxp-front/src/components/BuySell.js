cc8093ef47e739faade69076a21c66cef01a107d
Leticia <fmsornellas@gmail.com>
2022-07-22 00:59:03 -0300

    feat: added alert to not let client buy/sell negative amounts of asset

--- frontend/pselxp-front/src/components/BuySell.js
+++ frontend/pselxp-front/src/components/BuySell.js
@@ -1,79 +1,90 @@
 import React, { useState } from "react";
 import { useProvider } from "../context/provider";
 import axios from "axios";
+import swal from "sweetalert";
 
 function BuySell() {
   const [assetManipulation, setAssetManipulation] = useState({buy: 0, sell: 0});
-  const { wantedStock, clientData, setStockList, setClientData } = useProvider();
+  const { balance, wantedStock, clientData, setStockList, setClientData } = useProvider();
 
   function handleChange({ target }) {
-    if (target.id === 'buy') {
+    if (target.id === 'buy-input') {
       setAssetManipulation({ buy: target.value, sell: assetManipulation.sell });
     };
-    if (target.id === 'sell') {
+    if (target.id === 'sell-input') {
       setAssetManipulation({ buy: assetManipulation.buy, sell: target.value });
     };
   };
 
   function handleClick(event) {
     event.preventDefault();
-    if (event.target.id === 'button-buy') {
-      axios.put(`http://localhost:3009/stocks/${wantedStock.id}`,
+    const { target } = event;
+
+    console.log(assetManipulation[target.id]);
+
+    if (assetManipulation[target.id] <= 0) {
+      swal({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Insira algum valor adequado!',
+      });
+    };
+
+    if (target.value > 0) {
+      if (target.id === 'button-buy' && (target.value * wantedStock.value) > balance) {
+        axios.put(`http://localhost:3009/stocks/${wantedStock.id}`,
           {
             purchaseAmount: parseInt(assetManipulation.buy),
             clientId: clientData.id,
-          }
-        )
+          }).then(res => {
+            console.log(res.data);
+          }).catch(err => {
+            console.log(err);
+          });
+      };
+
+      if (target.id === 'button-sell' && clientData.stocks[wantedStock.id] > target.value) {
+        console.log(`VENDY ${assetManipulation.sell} AÇÕES`);
+        console.log(-assetManipulation.sell)
+        axios.put(`http://localhost:3009/stocks/${wantedStock.id}`,
+          {
+            purchaseAmount: parseInt(- assetManipulation.sell),
+            clientId: clientData.id,
+          }).then(res => {
+            console.log(res.data);
+          }).catch(err => {
+            console.log(err);
+          });
+      };
+
+      axios.get(`http://localhost:3009/clients/${clientData.email}`)
         .then(res => {
-          console.log(res.data);
+          setClientData(res.data);
         }).catch(err => {
           console.log(err);
-        });
-    };
+      });
 
-    if (event.target.id === 'button-sell') {
-      console.log(`VENDY ${assetManipulation.sell} AÇÕES`);
-      console.log(-assetManipulation.sell)
-      axios.put(`http://localhost:3009/stocks/${wantedStock.id}`,
-        {
-          purchaseAmount: parseInt(- assetManipulation.sell),
-          clientId: clientData.id,
-        }
-      )
+      axios.get('http://localhost:3009/stocks')
         .then(res => {
-          console.log(res.data);
+          setStockList(res.data);
         }).catch(err => {
           console.log(err);
-        });
+      });
     };
-
-    axios.get(`http://localhost:3009/clients/${clientData.email}`)
-      .then(res => {
-        setClientData(res.data);
-      }).catch(err => {
-        console.log(err);
-    });
-
-    axios.get('http://localhost:3009/stocks')
-      .then(res => {
-        setStockList(res.data);
-      }).catch(err => {
-        console.log(err);
-    });
   };
   
   return (
     <form>
       <div>
-        <label htmlFor='buy'>
+        <label htmlFor='buy-input'>
           <button
-            id='button-buy'
+            id='buy'
             onClick={ handleClick }
           >
             Comprar
           </button>
           <input
-            id='buy'
+            id='buy-input'
             type='number'
             value={ assetManipulation.buy }
             onChange={ handleChange }
@@ -83,15 +94,15 @@ function BuySell() {
       </div>
       {wantedStock.clientStock ? 
         <div>
-          <label htmlFor='sell'>
+          <label htmlFor='sell-input'>
             <button
-              id='button-sell'
+              id='sell'
               onClick={ handleClick }
             >
               Vender
             </button>
             <input
-              id='sell'
+              id='sell-input'
               type='number'
               value={ assetManipulation.sell }
               onChange={ handleChange }
