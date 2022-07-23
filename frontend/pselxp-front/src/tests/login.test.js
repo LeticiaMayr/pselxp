e8ab859e70b48e1a8821693dfd1718eea4890a36
Leticia <fmsornellas@gmail.com>
2022-07-23 16:21:32 -0300

    refactor: started testing and remove console.logs from api file

---
+++ frontend/pselxp-front/src/tests/login.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import App from '../App';
+// const { render, screen } = require('@testing-library/react');
+
+// describe('Tests application login page.', () => {
+//   test('Verify if inputs and button is rendering.', () => {
+//     render(<App />);
+//     const allInputs = screen.getAllByPlaceholderText();
+//     console.log(allInputs);
+//   });
+// });
