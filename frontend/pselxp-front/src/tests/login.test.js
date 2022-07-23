46bdf96475be1187754de1b942880233262f3782
Leticia <fmsornellas@gmail.com>
2022-07-23 17:52:38 -0300

    chore: more tests to se if inputs are updating properly

--- frontend/pselxp-front/src/tests/login.test.js
+++ frontend/pselxp-front/src/tests/login.test.js
@@ -2,12 +2,42 @@ import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 const { screen } = require('@testing-library/react');
 
 describe('Tests application login page.', () => {
-  test('Verify if inputs and button is rendering.', () => {
+
+  test('Verify if button and button is rendering.', () => {
     renderWithRouter(<App />);
+
     const loginButton = screen.getByText('Acessar');
+
     expect(loginButton).toBeInTheDocument();
+  });
+
+  test('Verify if inputs are rendering.', () => {
+    renderWithRouter(<App />);
+
+    const emailInput = screen.getByPlaceholderText('E-mail');
+    const passwordInput = screen.getByPlaceholderText('Senha');
+
+    expect(emailInput && passwordInput).toBeInTheDocument();
+  });
+
+  test('Verify if inputs are updating when user type something inside them.', () => {
+    renderWithRouter(<App />);
+
+    const randomSentence = 'Esse é meu jeito ninja';
+
+    const emailInput = screen.getByPlaceholderText('E-mail');
+    const passwordInput = screen.getByPlaceholderText('Senha');
+
+    userEvent.type(emailInput, randomSentence);
+    userEvent.type(passwordInput, 'de ser!!!!');
+
+    const expectedRandomSentence = randomSentence.replaceAll(' ', '');
+
+    expect(emailInput).toHaveValue(expectedRandomSentence);
+    expect(passwordInput).toHaveValue('de ser!!!!');
   });
 });
